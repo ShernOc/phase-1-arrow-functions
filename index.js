7f2e816abd3c81b828d23ce2bdeddc05b170080a
Sherlyne <sherlynea1994@hotmail.com>
2024-07-08 04:04:55 +0300

    modified Arrow function lab

---
+++ index.js
@@ -0,0 +1,25 @@
+// Arro functions 
+function divide (){
+  let a = 2000
+  let b = 100
+  return a/b 
+}
+divide(); 
+
+/* Arrow function 
+// function squares(x){
+//   let x =2 
+//   return x * x ; 
+ }*/
+
+
+const square = x =>x*x ; 
+let x = 2; 
+square(); 
+
+// Arrow add 
+const add= (a,b) => a +b ; 
+ let a = 3
+ let b = 4
+
+add(); 
